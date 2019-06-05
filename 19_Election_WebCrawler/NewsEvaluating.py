ad756d71321454f72e46d4cf784a4aee929fc591
paisano55@o365.ice.go.kr <paisano55@o365.ice.go.kr>
2019-06-05 17:24:51 +0900

    Sentiment Analyzer ADD

--- 19_Election_WebCrawler/NewsEvaluating.py
+++ 19_Election_WebCrawler/NewsEvaluating.py
@@ -1,8 +1,17 @@
 import json
 
-class KnuSL():
+class Sentiment():
 
     def data_list(wordname):
         with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
-			data = json.load(f)
+            data = json.load(f)
         result=[None,None]
+        for i in range(len(data)):
+            if data[i]['word']==wordname:
+                result.clear()
+                result.append(data[i]['word_root'])
+                result.append(data[i]['polarity'])
+        return result[0],result[1]
+
+       
+        
