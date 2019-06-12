278b81d503433c6e4a0aaa97bc703d8482b3baf9
paisano55@o365.ice.go.kr <paisano55@o365.ice.go.kr>
2019-06-12 17:46:59 +0900

    sync

--- 19_Election_WebCrawler/NewsEvaluating.py
+++ 19_Election_WebCrawler/NewsEvaluating.py
@@ -9,17 +9,14 @@ class Sentiment:
     def data_list(wordname):
         with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
             data = json.load(f)
-        result = [None, None]
+        result = None
         for i in range(len(data)):
             if data[i]['word'] == wordname:
-                result.clear()
-                result.append(data[i]['word_root'])
-                result.append(data[i]['polarity'])
-        return result[0], result[1]
+                result = data[i]['polarity')
+        return result
 
     def total_senti(self, article):
         cost = 0
-        # 무언가 분석
         return cost
 
 
@@ -37,18 +34,18 @@ class Analyzer:  # 형태소 자르기
         else:
             with open(filename, encoding='utf-8', mode='r') as f:
                 data = csv.reader(f)
-        for line in tqdm(data,desc=f" {Morpheme} ":
+        for line in tqdm(data, desc=f" {Morpheme} ":
             self.morph.append(self.analyzer(line))
 
     def cost_write(self, filename):
-        senti = Sentiment()
+        senti=Sentiment()
         if(filename == None):
             print("No File")
             exit
         else:
             with open(filename, encoding='utf-8', mode='w') as f:
-                data = csv.writer(f)
-        for i in tqdm(len(data),desc=f" {Sentiment} "):
+                data=csv.writer(f)
+        for i in tqdm(len(data), desc=f" {Sentiment} "):
             data[i].append(senti.total_senti(self.morph[i]))
 
 
