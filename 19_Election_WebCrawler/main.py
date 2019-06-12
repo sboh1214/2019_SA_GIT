5e7455446a2a278f4483010a8eed43b7e12fd825
오승빈 <sboh1214@gmail.com>
2019-06-12 16:00:27 +0900

    Apply keyword list

--- 19_Election_WebCrawler/main.py
+++ 19_Election_WebCrawler/main.py
@@ -2,8 +2,9 @@ from NewsCrawling import *
 from NewsEvaluating import *
 from NewsLearning import *
 
-Keyword = "대선"
-for i in range(1, 101):
-    api = NaverNewsAPI()
-    print("Request ("+str(i)+") : "+api.RequestNewsLink(Keyword, i))
+Keyword = ["대선"]
+for keyword in Keyword:
+    for i in range(1, 101):
+        api = NaverNewsAPI()
+        print("Request ("+str(i)+") : "+api.RequestNewsLink(keyword, i))
     
