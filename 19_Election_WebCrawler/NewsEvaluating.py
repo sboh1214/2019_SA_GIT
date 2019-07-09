2555e55825950d83b81226d8a4506b24558cfb61
paisano55@o365.ice.go.kr <paisano55@o365.ice.go.kr>
2019-07-09 18:18:21 +0900

    total_senti change

--- 19_Election_WebCrawler/NewsEvaluating.py
+++ 19_Election_WebCrawler/NewsEvaluating.py
@@ -1,61 +1,46 @@
 import json
 import csv
-import NewsLearning as NL
+from NewsLearning import MorphAnalyzer
 from tqdm import tqdm
 
 
 class Sentiment:
-    """
 
-    """
-
-    def data_list(wordname):
-        """
-
-        """
+    def senti_dict(wordname):
         with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
             data = json.load(f)
-        result = None
+        result = 0
         for i in range(len(data)):
             if data[i]['word'] == wordname:
                 result = data[i]['polarity']
         return result
 
-    def total_senti(self, article):
+    def total_senti(self,sentence):
         cost = 0
+        morph=MorphAnalyzer.GetMorph(sentence)
+        for key in morph.keys():
+            cost+=senti_dict(key)
         return cost
 
 
 class Analyzer:  # 형태소 자르기
-    """
-
-    """
     morph = []
     senti = Sentiment()
-
+    
     def analyzer(self, line):
-        """
-
-        """
         return GetMorpheme(line[3])
 
-    def morph_process(self, filename):
-        """
-
-        """
+    def morph_process(self, filename): #morph list에 
         if(filename == None):
             print("No File")
             exit
         else:
             with open(filename, encoding='utf-8', mode='r') as f:
                 data = csv.reader(f)
-        for line in tqdm(data, desc=f" {Morpheme} "):
+        for line in tqdm(data, desc=f" {Morpheme} ":
             self.morph.append(self.analyzer(line))
 
     def cost_write(self, filename):
-        """
-
-        """
         senti=Sentiment()
         if(filename == None):
             print("No File")
