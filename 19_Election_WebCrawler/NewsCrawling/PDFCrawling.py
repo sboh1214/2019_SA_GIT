ebb9c547f289c4315c7d141764a35c928282babb
Seungjae Kim <webmaster@ksjit.com>
2019-09-04 17:38:25 +0900

    Update NewsCrawling to 'New Pickle Standard'

--- 19_Election_WebCrawler/NewsCrawling/PDFCrawling.py
+++ 19_Election_WebCrawler/NewsCrawling/PDFCrawling.py
@@ -18,42 +18,42 @@ import re
 class ParsePDF:
     threadCount = 4
 
-    def text(self, parsedText):
-        parsedText = re.sub('\n', '', parsedText)
-        parsedText = re.sub(r'\([^)]*\)', '', parsedText)
+    def text(self, parsed_text):
+        parsed_text = re.sub('\n', '', parsed_text)
+        parsed_text = re.sub(r'\([^)]*\)', '', parsed_text)
         try:
-            parsedText = re.search('본회의를 개의하겠습니다.(.*)산회를 선포합니다.', parsedText).group(1)
+            parsed_text = re.search('본회의를 개의하겠습니다.(.*)산회를 선포합니다.', parsed_text).group(1)
         except AttributeError:
             print("본회의가 개의되지 않았거나 내가 Regex 잘못 씀.")
-        parsedText = parsedText.split('◯')
-        returnText = list()
-        for personText in parsedText:
-            speakerName = personText.split()[:2]
-            personTextList = list()
+        parsed_text = parsed_text.split('◯')
+        return_text = list()
+        for personText in parsed_text:
+            speaker_name = personText.split()[:2]
+            person_text = list()
             for txt in personText.split('.'):
-                personTextList.append(txt.split()[2:])
-            returnText.append([speakerName, personTextList])
-        return returnText
+                person_text.append(txt.split()[2:])
+            return_text.append([speaker_name, person_text])
+        return return_text
 
-    def readPDF(self, fileName='1.PDF'):
+    def read_pdf(self, file_name='1.PDF'):
         try:
-            file_data = tikaParse.from_file(fileName)  # Parse data from file
-            textData = file_data['content']  # Get file's text content
+            file_data = tikaParse.from_file(file_name)  # Parse data from file
+            text_data = file_data['content']  # Get file's text content
         except IOError as e:
             print("Error parsing PDF per next line : ")
             print(e)
             return " "
-        return self.text(textData)
+        return self.text(text_data)
 
-    def readFolder(self, dirName='../Data/'):  # Multithreaded Read Operations
-        directories = glob.glob("../Data/*.PDF")
+    def read_folder(self, dir_name="../Data/*.PDF"):  # Multithreaded Read Operations
+        directories = glob.glob(dir_name)
         pool = Pool(self.threadCount)
-        results = pool.map(self.readPDF, directories)
+        results = pool.map(self.read_pdf, directories)
         with open('../parsedPDF.txt', 'wb') as f:
-            pickle.dump(results,f)
+            pickle.dump(results, f)
 
 
 if __name__ == "__main__":
     parser = ParsePDF()
-    print(parser.readPDF('../Data/1.PDF'))  # Default is 1.PDF
-    parser.readFolder()
+    print(parser.read_pdf('../Data/1.PDF'))  # Default is 1.PDF
+    parser.read_folder()
