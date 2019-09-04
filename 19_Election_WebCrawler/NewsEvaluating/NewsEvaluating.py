230b3a3964b14c7585b6d1c0a750f256c7affbe3
iasa04 <iasa04@HyunMin-MacBook.local>
2019-09-04 17:28:13 +0900

    Pickle

--- 19_Election_WebCrawler/NewsEvaluating/NewsEvaluating.py
+++ 19_Election_WebCrawler/NewsEvaluating/NewsEvaluating.py
@@ -1,10 +1,15 @@
+''' 
+    튜플 리스트 (L/R,발언내용,단어별로 잘린 리스트)
+'''
+
 import json
 import csv
+import pickle
 from tqdm import tqdm
-from ..NewsLearning.NewsLearning import MorphAnalyzer
+from NewsLearning.NewsLearning import MorphAnalyzer
 
 
-class Sentiment:
+'''class Sentiment:
 
     def senti_dict(self, wordname):
         with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
@@ -20,12 +25,12 @@ class Sentiment:
         morph = MorphAnalyzer.getMorph(sentence)
         for key in morph.keys():
             cost += senti_dict(key)
-        return cost
+        return cost'''
 class Minutes:
     keyword={} #키워드 담는 이중 딕셔너리 keyword[키워드][L(좌파)/R(우파)]
-    minute=[] #회의록 담는 리스트가 담긴 튜플의 리스트 (L/R,발언내용)
+    minute=tuple() #튜플 리스트 (L/R,발언내용,단어별로 잘린 리스트)
     for comment in minute:
-        for index in range(len(comment[1])-1):
+        for index in range(len(comment[2])):
             for i in range(2,5): #iterator로 수정할 것
                 word=""
                 for j in range(index,index+i):
@@ -33,12 +38,7 @@ class Minutes:
                 if word not in keyword.keys():
                     keyword[word]={'L':0,'R':0}
                 keyword[word][minute[0]]+=1
-    
-
-
-
-
-class Analyzer:  # 형태소 자르기
+'''class Analyzer:  # 형태소 자르기
     morph = []
     senti = Sentiment()
 
@@ -64,7 +64,7 @@ class Analyzer:  # 형태소 자르기
             with open(filename, encoding='utf-8', mode='w') as f:
                 data = csv.writer(f)
         for i in tqdm(len(data), desc=f" {Sentiment} "):
-            data[i].append(senti.total_senti(self.morph[i]))
+            data[i].append(senti.total_senti(self.morph[i]))'''
 
 
 if __name__ == "__main__":
