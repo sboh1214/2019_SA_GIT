42701ed44167a3ad94382bcadd20a6a27b3446dd
paisano55@o365.ice.go.kr <paisano55@o365.ice.go.kr>
2019-09-11 17:03:45 +0900

    NewsTagging

--- 19_Election_WebCrawler/NewsEvaluating.py
+++ 19_Election_WebCrawler/NewsEvaluating.py
@@ -1,42 +1,37 @@
-"""
-    튜플 리스트 (L/R,발언내용,단어별로 잘린 리스트)
-"""
-
-'''class Sentiment:
-
-    def senti_dict(self, wordname):
-        with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
-            data = json.load(f)
-        result = 0
-        for i in range(len(data)):
-            if data[i]['word'] == wordname:
-                result = data[i]['polarity']
-        return result
-
-    def total_senti(self, sentence):
-        cost = 0
-        morph = MorphAnalyzer.getMorph(sentence)
-        for key in morph.keys():
-            cost += senti_dict(key)
-        return cost'''
-
-
+from .EvaluatingData import PdfList,NewsList
 class KeyWording:
-    def __init__(self, minute):
-        self.Minutes = minute  # minute[회의록 번호][[말한사람(L/R),[발언내용(단어 리스트)]]의 리스트]
+    def __init__(self):
+        self.pdfList=PdfList()
+        self.newsList=NewsList() # minute[회의록 번호][[말한사람(L/R),[발언내용(단어 리스트)]]의 리스트]
 
-    keyword = {}  # 키워드 담는 이중 딕셔너리 keyword[키워드][L(좌파)/R(우파)]
-    for minute in Minutes:
-        for comment in minute:
-            for word in range(len(comment[2]) - 1):
-                for i in range(2, 5):
-                    if i >= len(comment[2]): break
-                    word = ""
-                    for j in range(word, word + i):
-                        word += " " + minute[comment][1][j]
-                    if word not in keyword.keys():
-                        keyword[word] = {'L': 0, 'R': 0}
-                    keyword[word][minute[0]] += 1
+    keyword = {}
+    # 키워드 담는 이중 딕셔너리 keyword[키워드][L(좌파)/R(우파)]
+    def PdfKeywording(self):
+        Minutes=self.pdfList.importPickle()
+        for minute in Minutes:
+            for comment in minute:
+                for word in range(len(comment[2]) - 1):
+                    for i in range(2, 5):
+                        if i >= len(comment[2]): break
+                        word = ""
+                        for j in range(word, word + i):
+                            word += " " + minute[comment][1][j]
+                        if word not in self.keyword.keys():
+                            self.keyword[word] = {'L': 0, 'R': 0}
+                        self.keyword[word][minute[0]] += 1
+    
+    def NewsTagging(self):
+        News=self.newsList.importPickle()
+        for news in News:
+            allBias=0
+            for sentence in news['content']:
+                sentenceBias=0
+                for word in sentence:
+                    wordBias=self.keyword[word]['R']-self.keyword[word]['L'] #우편향일수록 양수. 중도가 0
+                    sentenceBias+=wordBias; allBias+=wordBias
+                    
+
+
 
 
 '''class Analyzer:  # 형태소 자르기
