4d8e25dbf492d309281591f71f8ec4212e87f815
Seungbin Oh <sboh1214@Seungbins-MacBook-Pro.local>
2019-10-23 12:33:52 +0900

    sync and delete .DS_Store

--- Test/NewsLearning.py
+++ Test/NewsLearning.py
@@ -1,9 +1,11 @@
+import itertools
 import matplotlib.pyplot as plt
-from keras import Sequential
-from keras.layers import *
+from keras import layers, models
 from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
 from tqdm import tqdm
+from math import sqrt, ceil
+from numpy import reshape
+import numpy as np
 
 from data import NewsList
 
@@ -28,119 +30,169 @@ class MorphAnalyzer:
 '''
 
 
-class NewsMLKeras:
+class Data:
     """
 
     """
-    NewsList = []
 
-    Rnn_X_Train = []
-    Rnn_Y_Train = []
-    Rnn_X_Test = []
-    Rnn_Y_Test = []
-
-    Cnn_X_Train = []
-    Cnn_Y_Train = []
-    Cnn_X_Test = []
-    Cnn_Y_Test = []
+    tokenizer = Tokenizer()
+    RnnX = []
+    RnnY = []
+    CnnX = []
+    CnnY = []
 
     Rnn_Model = None
     Cnn_Model = None
 
     History = None
 
+    def __init__(self, file, verbose=False, max_len=100):
+        self.Verbose = verbose
+        self.MaxLen = max_len
+        self.__get_news_data(filename=file)
+
     @staticmethod
-    def train_kor2vec(train=False):
-        pass
+    def __info(msg):
+        print('\033[35m'+msg+'\033[0m')
 
-    def get_news_data(self, filename: str = 'NewsData'):
+    @staticmethod
+    def __clean(sentences:list):
+        for i in range(len(sentences)):
+            sentences[i] = sentences[i].replace('\n','')
+        return sentences
+
+    @staticmethod
+    def __square(a):
+        output = []
+        length = ceil(sqrt(len(a)))
+        while True:
+            if len(a)>length:
+                output.append([a.pop() for _ in range(length)])
+            elif len(a)==length:
+                output.append([a.pop() for _ in range(length)])
+                return output
+            else:
+                output.append(a.extend([0 for _ in range(length-len(a))]))
+                return output
+
+    @staticmethod
+    def __pad(a, max_len):
+        if len(a)==max_len:
+            return a
+        elif len(a)>max_len:
+            return a[0:len(a)]
+        else:
+            return a.extend([0 for _ in range(max_len-len(a))])
+
+    def __get_news_data(self, filename: str = 'NewsData'):
         """
 
         """
-        news = NewsList()
-        news_list = news.importPickle(filename)
-        self.NewsList = news_list
-        train: list = news_list[:int(len(news_list) * 0.8)]
-        test: list = news_list[int(len(news_list) * 0.8):]
-        for data in tqdm(train):
-            for i, sentence in enumerate(data.Content):
-                self.Rnn_X_Train.append(" ".join(sentence))
-                self.Rnn_Y_Train.append(data.Sentence_Bias[i])
-                self.Cnn_Y_Test.append(data.Bias)
-        for data in tqdm(test):
-            for i, sentence in enumerate(data.Content):
-                self.Rnn_X_Test.append(" ".join(sentence))
-                self.Rnn_Y_Test.append(data.Sentence_Bias[i])
-                self.Cnn_Y_Test.append(data.Bias)
-        news.printCell()
+        self.__info('\nImport News List')
+        news_list = NewsList().importPickle(filename)
+        if self.Verbose:
+            print(news_list[0])
+        print(f'{len(news_list)} News Imported')
 
-    def build_rnn_model(self, max_features: int = 100):
-        """
+        self.__info('\nBuild Data : RnnX, RnnY, CnnX, CnnY')
+        for news in tqdm(news_list):
+            self.RnnX.extend(self.__clean(news.Content))
+            self.RnnY.extend(news.Sentence_Bias)
+            length = ceil(sqrt(len(news.Sentence_Bias)))
+            #self.CnnX.append(self.__square(news.Sentence_Bias))
+            self.CnnX.append(news.Sentence_Bias)
+            self.CnnY.append(news.Bias)
+        if self.Verbose:
+            self.__info(f'Rnn X ({len(self.RnnX)})')
+            print(self.RnnX[0])
+            self.__info(f'Rnn Y ({len(self.RnnY)})')
+            print(self.RnnY[0])
+            self.__info(f'Cnn X ({len(self.CnnX)})')
+            print(self.CnnX[0])
+            self.__info(f'Cnn Y ({len(self.CnnY)})')
+            print(self.CnnY[0])
 
-        """
-        model = Sequential()
-        model.add(Embedding(input_dim=max_features, output_dim=128))
-        model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-        model.add(Dense(1, activation='sigmoid'))
-        model.add(Dropout(0.5))
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.Rnn_Model = model
+        self.__info('\nPre-Process RnnX')
+        tokenizer = Tokenizer()
+        tokenizer.fit_on_texts(self.RnnX)
+        rnnXlist = tokenizer.texts_to_sequences(self.RnnX)
+        for i in tqdm(rnnXlist):
+            self.__pad(i, self.MaxLen)
+        rnnXarray=np.array([self.__pad(i, self.MaxLen) for i in rnnXlist])
+        self.RnnX = rnnXarray
+        if self.Verbose:
+            print(self.RnnX[0])
 
-    def build_cnn_model(self):
-        model = Sequential()
-        model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
-        model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-        model.add(MaxPool2D(pool_size=(2, 2)))
-        model.add(Dropout(rate=0.25))
-        model.add(Flatten())
-        model.add(Dense(units=128, activation='relu'))
-        model.add(Dropout(rate=0.5))
-        model.add(Dense(units=1, activation='relu'))
-        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        self.Cnn_Model = model
+class RNN(models.Model):
+    def __init__(self, max_len , data_count, max_features=20000):
+        x=layers.Input((max_len, ))
+        h=layers.Embedding(max_features, 128)(x)
+        h=layers.LSTM(128, dropout=0.2, recurrent_dropout= 0.2)(h)
+        y=layers.Dense(units=1, activation='sigmoid')(h)
+        super().__init__(x,y)
+        self.compile(loss='binary_crossentropy',optimizer='adam', metrics=['accuracy'])
 
-    def run_rnn_model(self, epochs: int = 1, batch_size: int = 10):
-        self.Rnn_Model.fit(x=self.Rnn_X_Test, y=self.Rnn_Y_Test,
-                           batch_size=batch_size,
-                           epochs=epochs,
-                           validation_split=0.2)
+class CNN(models.Model):
+    def __init__(self, input_shape):
+        x=layers.Input(shape=input_shape)
+        h=layers.Conv2D(filters=3, kernel_size=(3,3),activation='relu',input_shape=input_shape)(x)
+        h=layers.MaxPooling2D()(h)
+        h=layers.Dropout(rate=0.25)(h)
+        h=layers.Flatten()(h)
+        y=layers.Dense(units=1, activation='sigmoid')(h)
+        super().__init__(x,y)
+        self.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    def run_cnn_model(self, epochs: int = 1, batch_size: int = 10):
-        self.Cnn_Model.fit(x=self.Cnn_X_Test, y=self.Cnn_Y_Test,
-                           batch_size=batch_size,
-                           epochs=epochs,
-                           validation_split=0.2)
+class NewsML():
+    def __init__(self, rnn_maxlen=100 ,verbose=False):
+        self.Verbose = verbose
+        self.RnnMaxLen = rnn_maxlen
+        count = itertools.count(1)
+        
+        self.__info(f'[{next(count)}] Prepare Data')
+        self.Data = Data(file='NewsData', max_len=rnn_maxlen ,verbose=self.Verbose)
+
+        self.__info(f'[{next(count)}] Build RNN Model')
+        self.Rnn = RNN(max_len=self.RnnMaxLen,data_count=len(self.Data.RnnX),max_features=20000)
+
+        self.__info(f'[{next(count)}] Build CNN Model')
+        #self.Cnn = CNN()
+
+        self.__info(f'[{next(count)}] Run RNN Model')
+        self.RnnHistory = self.Rnn.fit(x=self.Data.RnnX, y=self.Data.RnnY, batch_size=256, epochs=5, validation_split=0.2, verbose=self.Verbose)
+
+        self.__info(f'[{next(count)}] Run CNN Model')
+        #self.CnnHistory = self.Cnn.fit(x=self.Data.CnnX, y=self.Data.CnnY, batch_size=128, epochs=5, validation_split=0.2, verbose=self.Verbose)
 
     def plot_loss_and_accuracy(self, name="", show=False):
         """
         Plot history for loss and accuracy of train and validation data.
         """
         plt.figure()
-        plt.subplot((4, 1, 1))
-        plt.plot(self.History.history['loss'])
-        plt.plot(self.History.history['val_loss'])
+        plt.subplot(2, 1, 1)
+        plt.plot(self.RnnHistory.history['loss'])
+        plt.plot(self.RnnHistory.history['val_loss'])
         plt.title(name + " RNN Loss")
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
-        plt.legend(['Train', 'Test'], loc=0)
+        plt.legend(['Train', 'Validation'], loc=0)
 
-        plt.subplot((4, 1, 2))
-        plt.plot(self.History.history['acc'])
-        plt.plot(self.History.history['val_acc'])
-        plt.title(name + " CNN Accuracy")
-        plt.xlabel('Epoch')
-        plt.ylabel('Accuracy')
-        plt.legend(['Train', 'Test'], loc=0)
+        # plt.subplot(2, 1, 2)
+        # plt.plot(self.RnnHistory.history['acc'])
+        # plt.plot(self.RnnHistory.history['val_acc'])
+        # plt.title(name + " RNN Accuracy")
+        # plt.xlabel('Epoch')
+        # plt.ylabel('Accuracy')
+        # plt.legend(['Train', 'Test'], loc=0)
 
         if show:
             plt.show()
 
+    @staticmethod
+    def __info(msg):
+        print('\n\033[35m'+msg+'\033[0m')
+    
 
 if __name__ == '__main__':
-    newsML = NewsMLKeras()
-    newsML.train_kor2vec(train=True)
-    newsML.get_news_data(filename='NewsData')
-    newsML.build_rnn_model()
-    newsML.build_cnn_model()
-    newsML.run_rnn_model()
-    newsML.run_cnn_model()
+    ml = NewsML(verbose=True)
+    ml.plot_loss_and_accuracy(name='Test', show=True)
