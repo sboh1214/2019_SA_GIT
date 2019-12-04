f420ec105d0ceccb02c842835423bc35ea80cbb0
Seungbin Oh <sboh1214@gmail.com>
2019-12-04 17:12:53 +0900

    sync

--- Test/NewsLearning.py
+++ Test/NewsLearning.py
@@ -133,7 +133,7 @@ class RNN(models.Model):
     def __init__(self, max_len, max_features=20000):
         x = layers.Input(shape=(max_len,))
         h = layers.Embedding(max_features, 128)(x)
-        h = layers.CuDNNLSTM(128, return_sequences=True)(h)
+        h = layers.CuDNNLSTM(128, return_sequences=False)(h)
         h = layers.Dropout(rate=0.2)(h)
         y = layers.Dense(units=1, activation=None)(h)
         super().__init__(x, y)
