018c70b531fffe29dbd23eddac52d3eb152635c8
Seungbin Oh <sboh1214@gmail.com>
2019-12-04 21:42:12 +0900

    sync

--- Test/NewsLearning.py
+++ Test/NewsLearning.py
@@ -140,7 +140,7 @@ class RNN(models.Model):
         h = layers.Dense(units=1)(h)
         y = layers.Dropout(rate=0.2)(h)
         super().__init__(x, y)
-        self.compile(loss=losses.BinaryCrossentropy(), optimizer=optimizers.Adam(learning_rate=0.0001),
+        self.compile(loss=losses.MeanSquaredError(), optimizer=optimizers.Adam(learning_rate=0.1),
                      metrics=['binary_accuracy', rms])
 
 
